1b58fe3dab7168416bfd1ed2eb5655510914c220
carlostic26 <carlospenaranda26@gmail.com>
2024-04-26 09:37:05 -0500

    error de duplicado de app solucionado

--- app/src/main/java/com/blogspot/robotaltavoz/ads/ads.kt
+++ app/src/main/java/com/blogspot/robotaltavoz/ads/ads.kt
@@ -8,8 +8,7 @@ class Ads {
     val openApp: String = "ca-app-pub-4336409771912215/4138844889"
 
 
-/*
-  // Test
+/*  // Test
   val banner: String = "ca-app-pub-3940256099942544/9214589741"
   val interstitial: String = "ca-app-pub-3940256099942544/1033173712"
   val openApp: String = "ca-app-pub-3940256099942544/9257395921"*/
